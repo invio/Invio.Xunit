968a0b6211b661f2d3e443e32ca339b4a2de6ab3
Brian Caruso <carusobp@gmail.com>
2017-07-21 12:01:38 -0700

    Add & utilize Invio.Xunit.sln

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-dotnet restore ./src/Invio.Xunit
-dotnet restore ./test/Invio.Xunit.Tests
+dotnet restore
 
 dotnet test ./test/Invio.Xunit.Tests/Invio.Xunit.Tests.csproj -c Release -f netcoreapp1.0
 
-dotnet pack ./src/Invio.Xunit -c Release -o ../../artifacts
+dotnet pack -c Release -o ../../artifacts
